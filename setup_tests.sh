bc6163fcb9492c17abb45877d6899519960ae039
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-27 00:24:56 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -12,7 +12,7 @@ elif [ "$el_version" = "7" ]; then
 	docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 	DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 	docker logs $DOCKER_CONTAINER_ID
-	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y install automake make kernel-devel gcc"
+	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make kernel-devel gcc"
 	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "mkdir -p /lib/modules/$(uname -r)/extra"
 	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/src/kernels/3.10.0-514.21.1.el7.x86_64 /lib/modules/4.4.0-51-generic/build"
 	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /home/travis/build/chvalean/lis-next/hv-rhel7.x/hv/ ; bash -xe rhel7-hv-driver-install"
