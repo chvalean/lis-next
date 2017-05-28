6e0f1fcc52785229ec08c9baae4e1ee6db1e99ec
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-28 14:29:48 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -10,7 +10,7 @@ if [ "$el_version" = "6" ]; then
 	sudo docker run --rm=true -v `pwd`:/home/travis/build/chvalean/lis-next:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe ls /root/"
 elif [ "$el_version" = "7" ]; then
 	#docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   /usr/sbin/init
-	docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   /sbin/init
+	sudo docker run --rm=true -v `pwd`:/home/travis/build/chvalean/lis-next:rw ${CENTOS} /bin/bash -c "bash -xe ls /root/"
 	DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 	docker logs $DOCKER_CONTAINER_ID
 	docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cat /etc/centos-release"
