a0e4364e22bc6fc4484d8af1ab6afed613632f7d
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-29 14:05:04 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -13,17 +13,19 @@ elif [ "$el_version" = "7" ]; then
 	DOCKER_CONTAINER_ID=$(sudo docker ps | grep centos | awk '{print $1}')
 	docker logs $DOCKER_CONTAINER_ID
 	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "cat /etc/centos-release"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make gcc wget"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://vault.centos.org/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "rpm -ivh kernel-devel-${KERNEL}.el7.x86_64.rpm"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "mkdir -p /lib/modules/$(uname -r)/extra"
-	# work-around to skip warning during install, we won't boot the new kernel
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "touch /lib/modules/$(uname -r)/modules.order"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "touch /lib/modules/$(uname -r)/modules.builtin"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make gcc wget"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://vault.centos.org/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "rpm -ivh kernel-devel-${KERNEL}.el7.x86_64.rpm"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "mkdir -p /lib/modules/$(uname -r)/extra"
 	
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/src/kernels/${KERNEL}.el7.x86_64 /lib/modules/4.4.0-51-generic/build"
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /home/travis/build/chvalean/lis-next/hv-rhel7.x/hv/ ; bash -e rhel7-hv-driver-install"
+	# work-around to skip warning during install, we won't boot the new kernel
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "touch /lib/modules/$(uname -r)/modules.order"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "touch /lib/modules/$(uname -r)/modules.builtin"
+	
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/src/kernels/${KERNEL}.el7.x86_64 /lib/modules/4.4.0-51-generic/build"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "bash -e /home/travis/build/chvalean/lis-next/hv-rhel7.x/hv/rhel7-hv-driver-install"
+	
 	docker stop $DOCKER_CONTAINER_ID
 	docker rm -v $DOCKER_CONTAINER_ID
 fi
