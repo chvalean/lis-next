2b14ab70e9a2fd8158679a7cc657a5d1588a510b
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-29 14:30:30 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -9,11 +9,14 @@ el_version="7"
 if [ "$el_version" = "6" ]; then
 	sudo docker run --rm=true -v `pwd`:/home/travis/build/chvalean/lis-next:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe ls /root/" 
 elif [ "$el_version" = "7" ]; then
-	docker run --privileged -d -t -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   tail -f /dev/null
+	# we need to start a process in foreground in order to keep the container running
+	docker run --privileged -d -t -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   /bin/bash
+
 	DOCKER_CONTAINER_ID=$(sudo docker ps | grep centos | awk '{print $1}')
 	docker logs $DOCKER_CONTAINER_ID
+
 	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "cat /etc/centos-release"
-	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make gcc wget"
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make gcc module-init-tools wget"
 	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://vault.centos.org/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
 	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
 	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "rpm -ivh kernel-devel-${KERNEL}.el7.x86_64.rpm"
