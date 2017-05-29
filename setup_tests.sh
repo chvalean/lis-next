27f6a83074141eafeb930ec49e94445eeb6d6dcb
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-29 14:01:17 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -7,14 +7,12 @@ el_version="7"
 
  # Run tests in Container
 if [ "$el_version" = "6" ]; then
-	sudo docker run --rm=true -v `pwd`:/home/travis/build/chvalean/lis-next:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe ls /root/"
+	sudo docker run --rm=true -v `pwd`:/home/travis/build/chvalean/lis-next:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe ls /root/" 
 elif [ "$el_version" = "7" ]; then
-	cat /etc/centos-release
-	yum -y -q install automake make gcc wget
-	#docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   /usr/sbin/init
-	#DOCKER_CONTAINER_ID=$(sudo docker ps | grep centos | awk '{print $1}')
-	#docker logs $DOCKER_CONTAINER_ID
-	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cat /etc/centos-release"
+	docker run --privileged -d -t -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/home/travis/build/chvalean/lis-next:rw  ${CENTOS}   tail -f /dev/null
+	DOCKER_CONTAINER_ID=$(sudo docker ps | grep centos | awk '{print $1}')
+	docker logs $DOCKER_CONTAINER_ID
+	docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "cat /etc/centos-release"
 	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake make gcc wget"
 	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://vault.centos.org/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
 	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
@@ -26,6 +24,6 @@ elif [ "$el_version" = "7" ]; then
 	
 	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/src/kernels/${KERNEL}.el7.x86_64 /lib/modules/4.4.0-51-generic/build"
 	#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /home/travis/build/chvalean/lis-next/hv-rhel7.x/hv/ ; bash -e rhel7-hv-driver-install"
-	#docker stop $DOCKER_CONTAINER_ID
-	#docker rm -v $DOCKER_CONTAINER_ID
+	docker stop $DOCKER_CONTAINER_ID
+	docker rm -v $DOCKER_CONTAINER_ID
 fi
