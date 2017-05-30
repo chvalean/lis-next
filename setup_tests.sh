26debe75ef7d441465a2fe5c745c7fa7827510c5
Chris Valean <cvalean@cloudbasesolutions.com>
2017-05-30 13:23:01 +0300

    Update setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -13,7 +13,7 @@ docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "yum -y -q install automake m
 docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://vault.centos.org/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
 if [[ "$BUILD" == "7.3.1611" ]]; then
   docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
-elif [[ "$BUILD" == "6."* ]]; then
+elif [[ "$BUILD" == "6.8" ]]; then
   docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget -q http://mirror.centos.org/centos/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el6.x86_64.rpm"
 fi
 docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "rpm -ivh kernel-devel-${KERNEL}*"
